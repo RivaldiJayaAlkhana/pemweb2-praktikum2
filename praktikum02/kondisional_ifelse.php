57daf32ce88bdf7d715348ab02167dfe2f19ee4e
RivaldiJayaAlkhana <114461248+RivaldiJayaAlkhana@users.noreply.github.com>
2023-03-05 17:00:41 +0700

    Add files via upload

---
+++ praktikum02/kondisional_ifelse.php
@@ -0,0 +1,10 @@
+<?php
+$nasi = "biasa";
+
+if($nasi == "digoreng"){
+    echo "Ya, ini digoreng";
+} elseif($nasi == "dibakar"){
+    echo "Ya, ini dibakar";
+}else{
+    echo "ini cuma nasi biasa";
+}
---
+++ praktikum02/kondisional_ifelse2.php
@@ -0,0 +1,8 @@
+<?php
+$result = 3 + 4;
+
+if($result != 8){
+    echo "Jawabannya benar!";
+} elseif($result > 7){
+    echo "Tidak benar !";
+}
